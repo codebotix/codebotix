5521b22961740294d3e781835eeea59328991c6e
codebotix <138878146+codebotix@users.noreply.github.com>
2025-11-23 03:13:59 +0530

    Update contact information for demo classes

--- src/app/Temporary/Temporary.tsx
+++ src/app/Temporary/Temporary.tsx
@@ -15,7 +15,7 @@ const Temporary = () => {
                     Till then please check out our programs <Link className='underline font-semibold' target='blank' href={'https://online.codebotix.in/'}>here</Link>
                 </div>
                 <div>
-                    Contact us: 7358278116/8122019903 for demo classes
+                    Contact us for online demo classes: 91503 95889/73582 78116 for demo classes
                 </div>
             </div>
         </div>
